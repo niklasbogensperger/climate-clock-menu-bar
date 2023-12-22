9906b7cb9cfbb4d0849db8a918b7f13d63c0de76
Niklas Bogensperger <niklasbogensperger@users.noreply.github.com>
2023-12-23 00:17:24 +0100

    Revamp error handling
    - pass on the errors from function to function
    - display them to the user
    
    This fixes the problem with the script continuously restarting when there is no internet connection at the moment 24h since the last check have passed.

--- climate_clock.15m.py
+++ climate_clock.15m.py
@@ -50,19 +50,19 @@ def query_api():
         response = requests.get(API_URL)
         response.raise_for_status()
     except requests.RequestException as e:
-        print(f"Error querying API: {e}")
-        return None
+        error = f"Error querying API:\n↪︎ {e}"
+        return None, error
 
     try:
         data = response.json()
         deadline = data['data']['modules']['carbon_deadline_1']['timestamp']
-        return deadline
+        return deadline, None
     except json.JSONDecodeError as e:
-        print(f"Error decoding JSON response: {e}")
-        return None
+        error = f"Error decoding JSON response:\n↪︎ {e}"
+        return None, error
     except KeyError as e:
-        print(f"Error extracting deadline from response: {e}")
-        return None
+        error = f"Error extracting deadline from response:\n↪︎ {e}"
+        return None, error
 
 
 def read_cache():
@@ -72,10 +72,12 @@ def read_cache():
                 cache_data = json.load(file)
                 timestamp = cache_data.get('timestamp')
                 deadline = cache_data.get('deadline')
-                return timestamp, deadline
-    except (IOError, json.JSONDecodeError, KeyError) as e:
-        print(f"Error during cache read: {e}")
-        return None
+                return (timestamp, deadline), None
+        else:
+            raise FileNotFoundError("CACHE_FILE does not exist.")
+    except (IOError, FileNotFoundError, json.JSONDecodeError, KeyError) as e:
+        error = f"Error during cache read:\n↪︎ {e}"
+        return None, error
 
 
 def write_cache(timestamp, deadline):
@@ -83,29 +85,31 @@ def write_cache(timestamp, deadline):
         with open(CACHE_FILE, 'w') as file:
             cache_data = {'timestamp': timestamp, 'deadline': deadline}
             json.dump(cache_data, file)
-            return True
+            return True, None
     except IOError as e:
-        print(f"Error during cache write: {e}")
-        return False
+        error = f"Error during cache write:\n↪︎ {e}"
+        return False, error
 
 
 def get_or_update_deadline():
-    cache_data = read_cache()
-    if cache_data is not None:
-        timestamp, deadline = cache_data
+    cache = read_cache()
+    if cache[0] is not None:
+        timestamp, deadline = cache[0]
         # only query the API if the cache is older than a day
         if pendulum.now() - pendulum.parse(timestamp) < pendulum.duration(hours=24):
-            return deadline
+            return deadline, None
     else:
         deadline = None
+    error_cache = cache[1]
 
-    new_deadline = query_api()
+    new_deadline, error_api = query_api()
+    errors = f"{error_cache if error_cache is not None else ''}\n{error_api if error_api is not None else ''}".strip()
     if new_deadline is not None:
         write_cache(pendulum.now().isoformat(), new_deadline)
-        return new_deadline
+        return new_deadline, errors
 
     # always (try to) return cached value as a fallback should there be an error during updating
-    return deadline
+    return deadline, errors
 
 
 def calculate_countdown(deadline_str):
@@ -149,7 +153,7 @@ def title_string(countdown):
     return f"{years}{y_label} {days}{d_label} {hours:02}{h_label}"
 
 
-deadline = get_or_update_deadline()
+deadline, errors = get_or_update_deadline()
 if deadline is not None:
     countdown = calculate_countdown(deadline)
     print(f"{title_string(countdown)} | color=pink")
@@ -159,3 +163,9 @@ if deadline is not None:
     print('---')
     print("climateclock.world | href='https://climateclock.world'")
     print("#ActInTime | disabled=true font='SF Mono'")
+else:
+    print("climate clock | color=pink")
+if errors:
+    print('---')
+    print("Last error(s): | disabled = true")
+    print(f"{errors}| disabled = true")
